9cfaf16472b3e28ef73364a52a21ddd99f9eccaa
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-11-27 00:38:21 +0100

    Initial Commit

---
+++ Browser/BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserApp.swift
+//  Browser
+//
+//  Created by Mia Koring on 27.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
